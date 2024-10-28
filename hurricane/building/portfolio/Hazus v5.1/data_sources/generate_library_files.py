2f24b2b4e460fcc21ac63b68a43605d907304457
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-10-28 15:10:53 -0700

    Ruff: RET504
    - Unnecessary assignment to `MSE` before `return` statement.
    - Change implemented.

--- hurricane/building/portfolio/Hazus v5.1/data_sources/generate_library_files.py
+++ hurricane/building/portfolio/Hazus v5.1/data_sources/generate_library_files.py
@@ -580,9 +580,7 @@ def create_Hazus_HU_damage_and_loss_files():  # noqa: C901, D103, N802, PLR0912,
                     ]
 
                     if res_type == 'MSE':
-                        MSE = sum(eps**2.0)  # noqa: N806
-
-                        return MSE
+                        return sum(eps**2.0)  # noqa: N806
 
                     if res_type == 'max abs eps':
                         return max(abs(eps))
@@ -607,9 +605,7 @@ def create_Hazus_HU_damage_and_loss_files():  # noqa: C901, D103, N802, PLR0912,
                     ]
 
                     if res_type == 'MSE':
-                        MSE = sum(eps**2.0)  # noqa: N806
-
-                        return MSE
+                        return sum(eps**2.0)  # noqa: N806
 
                     if res_type == 'max abs eps':
                         return max(abs(eps))
