a75d982b2ed74b00402de4c97cdbee6a3ad3231f
Adam Zsarnoczay <33822153+zsarnoczay@users.noreply.github.com>
2025-02-03 18:10:16 -0800

    Extend SimCenter hurricane wind component library to include all wind pressure components from our initial publication. Also: - create its own folder under components - create a raw data CSV file and a metadata header - something to consider for other libraries that we expect to grow over time - add a simple generator script that turns raw data into the standard data format used in the DLML - SimCenter Wind Component files

---
+++ hurricane/building/component/SimCenter Wind Component Library/data_sources/generate_library_files.py
@@ -0,0 +1,70 @@
+"""Generates SimCenter Hurricane Wind Component Library files."""
+
+import json
+import pandas as pd
+
+def create_fragility_files():
+
+    # open the raw input data file    
+    input_data = pd.read_csv("fragility_source.csv")
+
+    # start by creating the fragility CSV
+    fragility_cols = [
+        "ID",
+        "Incomplete",
+        "Demand-Type",
+        "Demand-Unit",
+        "Demand-Offset",
+        "Demand-Directional",
+        "LS1-Family",
+        "LS1-Theta_0",
+        "LS1-Theta_1"
+    ]
+
+    fragility_data = input_data[fragility_cols].set_index('ID').sort_index().convert_dtypes()
+
+    fragility_data.to_csv("fragility.csv")
+
+    # now open the metadata header file
+    with open("fragility_header.json",'r') as f:
+        meta = json.load(f)
+
+    # create the component-specific metadata
+    meta_plus = {}
+    for row_id, row in input_data.iterrows():
+
+        component_meta = {
+            "Description": row["Meta-Description"],
+            "Comments": row["Meta-Comments"],
+            "SuggestedComponentBlockSize": row["Meta-SuggestedComponentBlockSize"],
+            "RoudUpToIntegerQuantity": row["Meta-RoundUpToIntegerQuantity"],
+            "Reference": [ref.strip() for ref in row["Meta-Reference"].split(',')],
+            "LimitStates": {
+                "LS1": {
+                    "DS1": {
+                        "Description": row["Meta-LimitStates-LS1-DS1-Description"]
+                    }
+                }
+            }
+        }
+
+        # make sure the references are included in the header
+        for ref in component_meta["Reference"]:
+
+            if ref not in meta["References"]:
+
+                print(f"Error, missing reference: {ref}")
+
+        meta_plus[row["ID"]] = component_meta
+
+    meta.update(meta_plus)
+
+    with open("fragility.json",'w') as f:
+        json.dump(meta,f, indent=2)
+
+def main():
+    """Generate SimCenter Hurricane Wind Component Library Files."""
+    create_fragility_files()
+
+if __name__ == '__main__':
+    main()
