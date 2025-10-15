2fa1fbc91c952e3189622342e849768eff7d5afa
Tshajlij Lee <tlee@degenkolb.com>
2025-10-15 09:28:41 -0700

    page layout adjustments

--- st_ui/main_page.py
+++ st_ui/main_page.py
@@ -2,14 +2,15 @@ import streamlit as st
 from auth.login_ui import render_login_panel
 from st_search.fuzzy_visuals import render_fuzzy_search
 
-st.set_page_config(
-    page_title="Damage and Loss Model Library",
-    page_icon="📊",
-    layout="wide"
-)
+
 def render_main_page() -> None:
     """
     Render the main page
     """
+    st.set_page_config(
+        page_title="Damage and Loss Model Library",
+        page_icon="📊",
+        layout="wide"
+        )
     render_fuzzy_search()
 
